4b03f22c2d9d84aea4fe5c653c000c190455f239
Kevin Wei <chenghaowei@gmail.com>
2019-11-15 14:01:23 -0500

    Add queue data structure

---
+++ src/queue.js
@@ -0,0 +1,57 @@
+class _Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  display() {
+    if (this.first === null) {
+      return;
+    }
+
+    let curr = this.first;
+    while (curr !== null) {
+      console.log({
+        data: curr.data,
+        next: curr.next ? curr.next.data : null,
+      });
+      curr = curr.next;
+    }
+  }
+
+  peek() {
+    return this.first ? this.first.data : null;
+  }
+
+  enqueue(data) {
+    let node = new _Node(data, null);
+    if (!this.first) {
+      this.first = node;
+    }
+    if (this.last) {
+      this.last.next = node;
+    }
+    this.last = node;
+  }
+
+  dequeue() {
+    let node = this.first;
+    if (node === null) {
+      return;
+    }
+    this.first = node.next;
+    if (node === this.last) {
+      this.last = null;
+    }
+    return node.data;
+  }
+}
+
+module.exports = Queue;
